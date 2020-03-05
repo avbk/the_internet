88909ba18b120a68aa27f9acd2f64d4f7bb0d23c
Alexander von Bremen-Kühne <a.vonbremen-kuehne@neusta.de>
2020-03-05 16:17:56 +0100

    Simple response
    A simple response can be mocked with any given statusCode.
    If none is given it will map to 200 <EMPTY>.

--- lib/src/the_internet_base.dart
+++ lib/src/the_internet_base.dart
@@ -1,18 +1,102 @@
 import 'package:http/http.dart';
 import 'package:http/testing.dart';
 
-class TheInternet {
-  MockClient _client;
+const _kDefaultCode = 200;
+const _kDefaultBody = "";
 
-  TheInternet() {
-    _client = MockClient(_handleRequest);
+class TheInternet {
+  final Map<String, MockedServer> _servers;
+
+  TheInternet() : _servers = {};
+
+  BaseClient createHttpClient() => MockClient(_handleRequest);
+
+  MockedServer mockServer(String baseUrl) {
+    _servers[baseUrl] = MockedServer._(baseUrl);
+    return _servers[baseUrl];
   }
 
   Future<Response> _handleRequest(Request request) async {
-    return Response("", 0);
+    final InternetRequest req = InternetRequest.fromHttp(request);
+    for (var server in _servers.values) {
+      final InternetResponse response = server._tryHandle(req);
+      if (response != null) {
+        return response.toHttp();
+      }
+    }
+
+    return null;
+  }
+}
+
+class MockedServer {
+  final String _baseUrl;
+  final Map<String, _CallHandler> _handlers;
+
+  MockedServer._(this._baseUrl) : _handlers = {};
+
+  void get(String pathRegex, {int code: _kDefaultCode}) {
+    _handlers[pathRegex] = _CallHandler(
+      "GET",
+      _baseUrl,
+      pathRegex,
+      (request, args) => InternetResponse(code: code),
+    );
   }
 
-  MockClient getHttpClient() {
-    return _client;
+  InternetResponse _tryHandle(InternetRequest request) {
+    for (var handler in _handlers.values) {
+      final InternetResponse response = handler._tryHandle(request);
+      if (response != null) {
+        return response;
+      }
+    }
+
+    return null;
   }
+}
+
+typedef _ResponseBuilder = InternetResponse Function(
+  InternetRequest request,
+  List<String> args,
+);
+
+class _CallHandler {
+  final String method;
+  final RegExp _regex;
+  final _ResponseBuilder _handler;
+
+  _CallHandler(this.method, String baseUrl, String pathRegex, this._handler)
+      : this._regex = RegExp("$baseUrl$pathRegex");
+
+  InternetResponse _tryHandle(InternetRequest request) {
+    final isSameMethod = request.method.toUpperCase() == method.toUpperCase();
+
+    if (isSameMethod && _regex.hasMatch(request.url)) {
+      return _handler(request, []);
+    } else {
+      return null;
+    }
+  }
+}
+
+class InternetRequest {
+  final String method;
+  final String url;
+
+  InternetRequest.fromHttp(Request request)
+      : this.method = request.method,
+        this.url = request.url.toString();
+}
+
+class InternetResponse {
+  final int code;
+  final String body;
+
+  InternetResponse({
+    this.code = _kDefaultCode,
+    this.body = _kDefaultBody,
+  });
+
+  Response toHttp() => Response(body, code);
 }
--- test/the_internet_test.dart
+++ test/the_internet_test.dart
@@ -1,3 +1,4 @@
+import 'package:http/http.dart';
 import 'package:http/testing.dart';
 import 'package:test/test.dart';
 import 'package:the_internet/the_internet.dart';
@@ -5,13 +6,37 @@ import 'package:the_internet/the_internet.dart';
 void main() {
   group('The internet ', () {
     TheInternet internet;
+    MockedServer server;
+    BaseClient client;
 
     setUp(() {
       internet = TheInternet();
+      server = internet.mockServer("https://example.com");
+      client = internet.createHttpClient();
     });
 
-    test('allows to obtain a httpcient', () {
-      expect(internet.getHttpClient(), isA<MockClient>());
+    test('allows to obtain a httpclient', () {
+      expect(client, isA<MockClient>());
+    });
+
+    test('uses 200 <EMPTY> as default', () async {
+      server.get("/nothing");
+
+      final response = await client.get("https://example.com/nothing");
+
+      expect(response.statusCode, 200);
+      expect(response.body, isEmpty);
+      expect(response.headers, isEmpty);
+    });
+
+    test('allows to create a very simple get mock', () async {
+      server.get("/messages/17", code: 404);
+
+      final response = await client.get("https://example.com/messages/17");
+
+      expect(response.statusCode, 404);
+      expect(response.body, isEmpty);
+      expect(response.headers, isEmpty);
     });
   });
 }
