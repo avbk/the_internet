e0065e96a9be9101986c1be8e19769120eeb1cdb
Alexander von Bremen-Kühne <a.vonbremen-kuehne@neusta.de>
2020-03-09 16:07:47 +0100

    Unified tests for POST
    All POST tests, which were implemented for the http client, are now migrated to
    multiClient-tests. Thus every test is executed for http as well as dio.

--- test/dio_specific_test.dart
+++ test/dio_specific_test.dart
@@ -4,7 +4,7 @@ import 'package:test/test.dart';
 import 'test_utils.dart';
 
 void main() {
-  dioClientTestGroup("GET", (test) {
+  dioClientTestGroup((test) {
     test("has a mockAdapter", (server, dio) {
       expect(dio.httpClientAdapter, isA<HttpClientAdapter>());
     });
@@ -21,14 +21,55 @@ void main() {
       final Response<Map> response =
           await dio.get("https://example.com/messages");
 
-      expect(response.statusCode, 200);
-      expect(response.data, {
-        "page": 1,
-        "messages": ["Hello", "World"],
-      });
-      expect(response.headers.map, {
-        "content-type": ["application/json"]
-      });
+          expect(response.statusCode, 200);
+          expect(response.data, {
+            "page": 1,
+            "messages": ["Hello", "World"],
+          });
+          expect(response.headers.map, {
+            "content-type": ["application/json"]
+          });
+        });
+
+    test("formdata header is added to request", (server, dio) async {
+      server.post("/messages", code: 204);
+
+      await dio.post(
+        "https://example.com/messages",
+        data: FormData.fromMap({"title": "Hello"}),
+      );
+
+      final recordedCall = server.nextCapturedCall();
+
+      expect(
+        recordedCall.request.headers["content-type"],
+        // ignore random boundary string
+        startsWith("multipart/form-data; boundary=--dio-boundary-"),
+      );
+      expect(
+        recordedCall.request.headers["content-length"],
+        "115",
+      );
+    });
+
+    test("json header is added to request", (server, dio) async {
+      server.post("/messages", code: 204);
+
+      await dio.post(
+        "https://example.com/messages",
+        data: {"title": "Hello"},
+      );
+
+      final recordedCall = server.nextCapturedCall();
+
+      expect(
+        recordedCall.request.headers["content-type"],
+        "application/json; charset=utf-8",
+      );
+      expect(
+        recordedCall.request.headers["content-length"],
+        "17",
+      );
     });
   });
 }
---
+++ test/post_test.dart
@@ -0,0 +1,71 @@
+import 'package:test/test.dart';
+import 'package:the_internet/the_internet.dart';
+
+import 'test_utils.dart';
+
+void main() {
+  multiClientTestGroup("POST", (test) {
+    test(
+      "can create very simple POST mock",
+      configure: (server) => server.post("/messages", code: 204),
+      request: {"path": "/messages"},
+      response: {"code": 204, "body": isEmpty, "headers": isEmpty},
+    );
+
+    test("formdata can be inspected afterwards",
+        configure: (server) => server.post("/messages", code: 204),
+        request: {
+          "path": "/messages",
+          "formData": {"title": "Hello"}
+        },
+        response: {
+          "code": 204,
+          "body": isEmpty,
+          "headers": isEmpty
+        },
+        recorded: {
+          "request": {
+            "url": "https://example.com/messages",
+            "bodyAsString": "title=Hello",
+            "bodyAsFormData": {"title": "Hello"},
+            "bodyAsJson": isNull
+          },
+          "response": {"code": 204, "body": isEmpty, "headers": isEmpty},
+        });
+
+    test("json can be inspected afterwards",
+        configure: (server) => server.post("/messages", code: 204),
+        request: {
+          "path": "/messages",
+          "json": {"title": "Hello"}
+        },
+        response: {
+          "code": 204,
+          "body": isEmpty,
+          "headers": isEmpty
+        },
+        recorded: {
+          "request": {
+            "url": "https://example.com/messages",
+            "bodyAsString": '{"title":"Hello"}',
+            "bodyAsFormData": isNull,
+            "bodyAsJson": {"title": "Hello"}
+          },
+          "response": {"code": 204, "body": isEmpty, "headers": isEmpty},
+        });
+
+    test("respond with input data",
+        configure: (server) => server.post("/messages",
+            body: (CapturedRequest request, _) =>
+                {"newMessage": "message ${request.body.asFormData["title"]}"}),
+        request: {
+          "path": "/messages",
+          "formData": {"title": "Hello"}
+        },
+        response: {
+          "code": 200,
+          "body": '{"newMessage":"message Hello"}',
+          "headers": {"Content-Type": "application/json"}
+        });
+  });
+}
